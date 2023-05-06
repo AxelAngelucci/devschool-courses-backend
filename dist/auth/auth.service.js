25977de2f2c354a10e4f0a7e8dba6d1b4c38d231
Axel Angelucci <axeljesusangelucci@gmail.com>
2023-05-06 17:20:14 -0300

    x

---
+++ dist/auth/auth.service.js
@@ -0,0 +1,56 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AuthService = void 0;
+const common_1 = require("@nestjs/common");
+const mongoose_1 = require("@nestjs/mongoose");
+const auth_schema_1 = require("./schema/auth.schema");
+const mongoose_2 = require("mongoose");
+const bcrypt_1 = require("bcrypt");
+const jwt_1 = require("@nestjs/jwt");
+let AuthService = class AuthService {
+    constructor(authModel, jwtService) {
+        this.authModel = authModel;
+        this.jwtService = jwtService;
+    }
+    async register(payload) {
+        const { password } = payload;
+        const hashPwd = await (0, bcrypt_1.hash)(password, 10);
+        payload = Object.assign(Object.assign({}, payload), { password: hashPwd });
+        return this.authModel.create(payload);
+    }
+    async login(payload) {
+        const { email, password } = payload;
+        const findUser = await this.authModel.findOne({ email });
+        if (!findUser)
+            throw new common_1.HttpException('Usuario o contraseña incorrectos', 404);
+        const checkPassword = await (0, bcrypt_1.compare)(password, findUser.password);
+        if (!checkPassword)
+            throw new common_1.HttpException('Usuario o contraseña incorrectos', 404);
+        const token = await this.jwtService.sign(JSON.stringify(findUser));
+        const data = {
+            user: findUser,
+            token,
+        };
+        return data;
+    }
+};
+AuthService = __decorate([
+    (0, common_1.Injectable)(),
+    __param(0, (0, mongoose_1.InjectModel)(auth_schema_1.Auth.name)),
+    __metadata("design:paramtypes", [mongoose_2.Model,
+        jwt_1.JwtService])
+], AuthService);
+exports.AuthService = AuthService;
+//# sourceMappingURL=auth.service.js.map
---
+++ dist/auth/dto/register-auth.dto.d.ts
@@ -0,0 +1,7 @@
+import { LoginAuthDto } from './login-auth.dto';
+declare const RegisterAuthDto_base: import("@nestjs/common").Type<Partial<LoginAuthDto>>;
+export declare class RegisterAuthDto extends RegisterAuthDto_base {
+    name: string;
+    phone: number;
+}
+export {};
